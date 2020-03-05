32b6941a2a291ee071c8d78b9950a020c02a7fd6
Ömer <55637993+Omer975@users.noreply.github.com>
2020-03-05 20:13:36 +0100

    added ımage

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Everybody</h1>
-      <h2>Welcome to over Academy</h2>
+      <h1>Nordic IT Garage Academy</h1>
+      <h2>Hello Everybody</h2>
+      <h2>Welcome to our Academy</h2>
+      <img src="https://i.postimg.cc/VvGFD5GJ/indir-1.jpg" alt="picture" />
     </div>
   );
 }
